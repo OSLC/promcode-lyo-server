0b81469e1ff1bb6970208d3a4ec7e0b27d2e6e7c
Andrew Berezovskyi <andrew@berezovskyi.me>
2025-07-12 17:16:28 +0200

    fix: CORS
    Proper CORS filter with auth support

--- promcode-lyo-server/src/main/java/org/oasis/oslc/promcode/server/services/ServiceProviderService2.java
+++ promcode-lyo-server/src/main/java/org/oasis/oslc/promcode/server/services/ServiceProviderService2.java
@@ -116,15 +116,6 @@ public class ServiceProviderService2
         super();
     }
 
-    private void addCORSHeaders (final HttpServletResponse httpServletResponse) {
-        //UI preview can be blocked by CORS policy.
-        //add select CORS headers to every response that is embedded in an iframe.
-        httpServletResponse.addHeader("Access-Control-Allow-Origin", "*");
-        httpServletResponse.addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD");
-        httpServletResponse.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-        httpServletResponse.addHeader("Access-Control-Allow-Credentials", "true");
-    }
-
     @OslcDialog
     (
          title = "SelectionDialog1",
